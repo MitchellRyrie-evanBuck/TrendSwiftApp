1b4b6f2cf7404f68bb5c3edc2edb6f259298b7e6
Mitchell Ryrie <liuxiaowen66621@gmail.com>
2024-09-28 23:42:24 +0800

    first commit

---
+++ TrendSwiftApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TrendSwiftApp
+//
+//  Created by L Af on 2024/9/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TrendSwiftApp/TrendSwiftAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrendSwiftAppApp.swift
+//  TrendSwiftApp
+//
+//  Created by L Af on 2024/9/28.
+//
+
+import SwiftUI
+
+@main
+struct TrendSwiftAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

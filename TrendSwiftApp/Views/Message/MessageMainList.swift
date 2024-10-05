bfd3129886bc6f46fab4a2a62bc9099fb6915f6d
Mitchell Ryrie <liuxiaowen66621@gmail.com>
2024-10-05 21:22:04 +0800

    feat: message
    Signed-off-by: Mitchell Ryrie <liuxiaowen66621@gmail.com>

---
+++ TrendSwiftApp/Views/Message/MessageMainList.swift
@@ -0,0 +1,18 @@
+//
+//  MessageMainList.swift
+//  TrendSwiftApp
+//
+//  Created by L Af on 2024/10/5.
+//
+
+import SwiftUI
+
+struct MessageMainList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MessageMainList()
+}

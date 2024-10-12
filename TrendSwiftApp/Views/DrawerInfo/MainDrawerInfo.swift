790b4c1ef809360abd572e6270c279750be47b4d
Mitchell Ryrie <liuxiaowen66621@gmail.com>
2024-10-12 23:39:15 +0800

    feat: Drawer
    Signed-off-by: Mitchell Ryrie <liuxiaowen66621@gmail.com>

---
+++ TrendSwiftApp/Views/DrawerInfo/MainDrawerInfo.swift
@@ -0,0 +1,18 @@
+//
+//  MainDrawerInfo.swift
+//  TrendSwiftApp
+//
+//  Created by L Af on 2024/10/12.
+//
+
+import SwiftUI
+
+struct MainDrawerInfo: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainDrawerInfo()
+}

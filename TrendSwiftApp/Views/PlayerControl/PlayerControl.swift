52ad01b94c81be8b0d66ccf191ef29d03fed4ce2
Mitchell Ryrie <liuxiaowen66621@gmail.com>
2024-10-31 21:25:41 +0800

    feat: Player
    Signed-off-by: Mitchell Ryrie <liuxiaowen66621@gmail.com>

---
+++ TrendSwiftApp/Views/PlayerControl/PlayerControl.swift
@@ -0,0 +1,18 @@
+//
+//  PlayerControl.swift
+//  TrendSwiftApp
+//
+//  Created by L Af on 2024/10/31.
+//
+
+import SwiftUI
+
+struct PlayerControl: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PlayerControl()
+}

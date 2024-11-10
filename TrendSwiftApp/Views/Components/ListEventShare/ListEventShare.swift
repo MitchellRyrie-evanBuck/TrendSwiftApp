d3c859574f291441668050f84ccf15d78301a080
Mitchell Ryrie <liuxiaowen66621@gmail.com>
2024-11-10 23:34:40 +0800

    feat: ListEventShare
    Signed-off-by: Mitchell Ryrie <liuxiaowen66621@gmail.com>

---
+++ TrendSwiftApp/Views/Components/ListEventShare/ListEventShare.swift
@@ -0,0 +1,18 @@
+//
+//  ListEventShare.swift
+//  TrendSwiftApp
+//
+//  Created by L Af on 2024/11/10.
+//
+
+import SwiftUI
+
+struct ListEventShare: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListEventShare()
+}

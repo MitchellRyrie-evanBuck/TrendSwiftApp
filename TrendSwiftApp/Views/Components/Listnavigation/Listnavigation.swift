17922ff205b35139c800a6f89877ae1983da9f70
Mitchell Ryrie <liuxiaowen66621@gmail.com>
2024-11-11 22:20:49 +0800

    feat: Listnavigation
    Signed-off-by: Mitchell Ryrie <liuxiaowen66621@gmail.com>

---
+++ TrendSwiftApp/Views/Components/Listnavigation/Listnavigation.swift
@@ -0,0 +1,18 @@
+//
+//  Listnavigation.swift
+//  TrendSwiftApp
+//
+//  Created by L Af on 2024/11/11.
+//
+
+import SwiftUI
+
+struct Listnavigation: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Listnavigation()
+}

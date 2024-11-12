c7673a23e90b34c506a2bdd5a0857e4b8b833dc4
Mitchell Ryrie <liuxiaowen66621@gmail.com>
2024-11-12 23:37:10 +0800

    MusicContainer
    Signed-off-by: Mitchell Ryrie <liuxiaowen66621@gmail.com>

---
+++ TrendSwiftApp/Views/Components/MusicContainer/MusicContainer.swift
@@ -0,0 +1,18 @@
+//
+//  MusicContainer.swift
+//  TrendSwiftApp
+//
+//  Created by L Af on 2024/11/12.
+//
+
+import SwiftUI
+
+struct MusicContainer: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MusicContainer()
+}

5fdad145e1dde253e62710810dc636b4fd70039e
Mitchell Ryrie <liuxiaowen66621@gmail.com>
2024-12-08 23:23:17 +0800

    feat: add User
    Signed-off-by: Mitchell Ryrie <liuxiaowen66621@gmail.com>

---
+++ TrendSwiftApp/Views/User/FixedHeaderC.swift
@@ -0,0 +1,18 @@
+//
+//  FixedHeaderC.swift
+//  TrendSwiftApp
+//
+//  Created by L Af on 2024/12/8.
+//
+
+import SwiftUI
+
+struct FixedHeaderC: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FixedHeaderC()
+}

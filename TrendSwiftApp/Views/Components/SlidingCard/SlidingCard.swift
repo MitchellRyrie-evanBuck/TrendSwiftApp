9e9b2a5a29b8aa604d6245324750ad85a58e3d2f
Mitchell Ryrie <liuxiaowen66621@gmail.com>
2025-01-01 22:43:55 +0800

    feat: SlidingCard
    Signed-off-by: Mitchell Ryrie <liuxiaowen66621@gmail.com>

---
+++ TrendSwiftApp/Views/Components/SlidingCard/SlidingCard.swift
@@ -0,0 +1,18 @@
+//
+//  SlidingCard.swift
+//  TrendSwiftApp
+//
+//  Created by L Af on 2025/1/1.
+//
+
+import SwiftUI
+
+struct SlidingCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SlidingCard()
+}

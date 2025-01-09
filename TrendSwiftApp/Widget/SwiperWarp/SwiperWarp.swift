4b339bc6a40970020f14b76ef276bada4ff4dff0
Mitchell Ryrie <liuxiaowen66621@gmail.com>
2025-01-09 23:56:55 +0800

    feat: SwiperWarp
    Signed-off-by: Mitchell Ryrie <liuxiaowen66621@gmail.com>

---
+++ TrendSwiftApp/Widget/SwiperWarp/SwiperWarp.swift
@@ -0,0 +1,18 @@
+//
+//  SwiperWarp.swift
+//  TrendSwiftApp
+//
+//  Created by L Af on 2025/1/9.
+//
+
+import SwiftUI
+
+struct SwiperWarp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiperWarp()
+}

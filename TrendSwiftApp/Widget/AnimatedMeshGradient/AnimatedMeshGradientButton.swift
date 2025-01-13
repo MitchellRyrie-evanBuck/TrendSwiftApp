07eb685d730211fa8025dcd0f2214ccb64d21088
Mitchell Ryrie <liuxiaowen66621@gmail.com>
2025-01-13 23:35:01 +0800

    🐸
    Signed-off-by: Mitchell Ryrie <liuxiaowen66621@gmail.com>

---
+++ TrendSwiftApp/Widget/AnimatedMeshGradient/AnimatedMeshGradientButton.swift
@@ -0,0 +1,51 @@
+//
+//  AnimatedMeshGradientButton.swift
+//  TrendSwiftApp
+//
+//  Created by L Af on 2025/1/13.
+//
+
+import SwiftUI
+
+struct AnimatedMeshGradientButton: View {
+    var body: some View {
+      
+      HStack {
+        Text("Generating...")
+          .foregroundStyle(.white)
+          .font(.title2)
+      }
+      .padding(.vertical, 16)
+      .frame(maxWidth: 370)
+      .background(
+        AnimatedMeshGradient()
+          .mask(RoundedRectangle(cornerRadius: 16)
+            .stroke(lineWidth: 16)
+            .blur(radius: 8)
+          )
+          .overlay{
+            RoundedRectangle(cornerRadius: 16)
+              .stroke(.white, lineWidth: 3)
+              .blur(radius: 2)
+              .blendMode(.overlay)
+          }
+          .overlay{
+            RoundedRectangle(cornerRadius: 16)
+              .stroke(.white, lineWidth: 1)
+              .blur(radius: 1)
+              .blendMode(.overlay)
+          }
+      )
+      .background(.black)
+      .cornerRadius(16)
+      .background(
+        RoundedRectangle(cornerRadius: 16)
+          .stroke(.black.opacity(0.5), lineWidth: 1)
+      )
+    }
+  
+}
+
+#Preview {
+    AnimatedMeshGradientButton()
+}

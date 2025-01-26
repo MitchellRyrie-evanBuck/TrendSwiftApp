d29622adaf4fca2c9db138a335663ac337640cbb
Mitchell Ryrie <liuxiaowen66621@gmail.com>
2025-01-26 22:19:59 +0800

    feat: surroundings
    Signed-off-by: Mitchell Ryrie <liuxiaowen66621@gmail.com>

---
+++ TrendSwiftApp/Views/Login/LoginMain.swift
@@ -0,0 +1,18 @@
+//
+//  LoginMain.swift
+//  TrendSwiftApp
+//
+//  Created by L Af on 2025/1/26.
+//
+
+import SwiftUI
+
+struct LoginMain: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginMain()
+}

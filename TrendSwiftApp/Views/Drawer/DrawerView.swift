512fd3003178cdf070cfa04ea737d2f411745846
Mitchell Ryrie <liuxiaowen66621@gmail.com>
2025-02-22 23:57:10 +0800

    feat: drawer
    Signed-off-by: Mitchell Ryrie <liuxiaowen66621@gmail.com>

---
+++ TrendSwiftApp/Views/Drawer/DrawerView.swift
@@ -0,0 +1,18 @@
+//
+//  DrawerView.swift
+//  TrendSwiftApp
+//
+//  Created by L Af on 2025/2/22.
+//
+
+import SwiftUI
+
+struct DrawerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DrawerView()
+}

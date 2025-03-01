5589d374ac78865387f54f17b1b6de686fd1681e
Mitchell Ryrie <liuxiaowen66621@gmail.com>
2025-03-01 23:51:43 +0800

    card
    Signed-off-by: Mitchell Ryrie <liuxiaowen66621@gmail.com>

---
+++ TrendSwiftApp/Views/SocialCard/SocialCard.swift
@@ -0,0 +1,18 @@
+//
+//  SocialCard.swift
+//  TrendSwiftApp
+//
+//  Created by L Af on 2025/3/1.
+//
+
+import SwiftUI
+
+struct SocialCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SocialCard()
+}
